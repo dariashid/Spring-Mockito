c82bdb84f121c7e84b9bf747a91cab227f58a9b1
dariashid <brbrbrbrbrbrbr@internet.ru>
2024-05-04 17:05:56 +0300

    /

---
+++ src/main/java/com/pro/sky/employeesaccounting/EmployeesaccountingApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.employeesaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesaccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesaccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/sky/employeesaccounting/EmployeesaccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.employeesaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesaccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
